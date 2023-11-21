df2240146c8a085fdbac60b69da3591859b57a0e
Hamza Bow <hamzabow77@gmail.com>
2023-11-21 01:18:00 +0100

    add Node struct

--- main.go
+++ main.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	nd := Node{
+		name: "Root",
+	}
+	fmt.Println(nd)
 }
---
+++ node.go
@@ -0,0 +1,6 @@
+package main
+
+type Node struct {
+	name        string
+	description string
+}
