75d918e2bbb57d614f19a60a1910e855a4981c5b
Hamza Bow <hamzabow77@gmail.com>
2023-11-22 13:46:53 +0100

    clean main file

--- main.go
+++ main.go
@@ -1,109 +1,12 @@
 package main
 
-import (
-	"fmt"
-	"log"
-)
+import "fmt"
 
 func main() {
-	// code1()
-	code2()
-	// code3()
-}
-
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-func code3() {
-	var x = map[rune]string{
-		'a': "abcd",
-	}
-	x['b'] = "bcde"
-	fmt.Println(x)
-}
-
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
-func code2() {
 	var rt = node{
 		name:        "Root",
 		description: "Desc of Root",
 		children:    map[rune]node{},
 	}
-	var ch1 = node{
-		parent:  &rt,
-		command: "google-chrome",
-	}
-	rt.children['a'] = ch1
-	fmt.Println(rt.children)
-	// rt.children['a'].run_command()
-	err := rt.run_command()
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println(rt)
 }
-
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
-func code1() {
-
-	// child := node{
-	// 	name:        "Child A",
-	// 	description: "Desc Ch A",
-	// }
-	nd := node{
-		name:        "ND",
-		description: "Desc ND",
-		command:     "google-chrome",
-	}
-	fmt.Println(nd.parent)
-	// cmd := exec.Command(nd.command)
-	// err := cmd.Run()
-	// if err != nil {
-	// 	log.Fatal("sddf", err)
-	// }
-}
-
-// func main() {
-// 	child_nd1 := node{
-// 		name: "Child A",
-// 	}
-// 	child_nd2 := node{
-// 		name: "Child B",
-// 	}
-// 	nd := node{
-// 		name: "Root",
-// 		children: map[rune]node{
-// 			'a': child_nd1,
-// 			'b': child_nd2,
-// 		},
-// 	}
-// 	fmt.Println(nd.children['b'])
-// 	fmt.Println(nd.children['b'].children)
-// 	fmt.Println(nd)
-// }
