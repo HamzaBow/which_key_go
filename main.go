0c3e2dba81976e4c12fbdecb3bc0f7f14ad037d8
Hamza Bow <hamzabow77@gmail.com>
2023-11-22 13:22:21 +0100

    add code for testing new 'features'

--- main.go
+++ main.go
@@ -3,20 +3,59 @@ package main
 import (
 	"fmt"
 	"log"
-	"os/exec"
 )
 
 func main() {
-	nd := node{
-		name:        "ND",
-		description: "Desc ND",
-		command:     "google-chrome",
+	// code1()
+	code2()
+	// code3()
+}
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+func code3() {
+	var x = map[rune]string{
+		'a': "abcd",
 	}
-	fmt.Println(nd)
-	cmd := exec.Command(nd.command)
-	err := cmd.Run()
+	x['b'] = "bcde"
+	fmt.Println(x)
+}
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+func code2() {
+	var rt = node{
+		name:        "Root",
+		description: "Desc of Root",
+		children:    map[rune]node{},
+	}
+	var ch1 = node{
+		parent:  &rt,
+		command: "google-chrome",
+	}
+	rt.children['a'] = ch1
+	fmt.Println(rt.children)
+	// rt.children['a'].run_command()
+	err := rt.run_command()
 	if err != nil {
-		log.Fatal("sddf", err)
+		log.Fatal(err)
 	}
 }
 
@@ -30,6 +69,25 @@ func main() {
 //
 //
 //
+
+func code1() {
+
+	// child := node{
+	// 	name:        "Child A",
+	// 	description: "Desc Ch A",
+	// }
+	nd := node{
+		name:        "ND",
+		description: "Desc ND",
+		command:     "google-chrome",
+	}
+	fmt.Println(nd.parent)
+	// cmd := exec.Command(nd.command)
+	// err := cmd.Run()
+	// if err != nil {
+	// 	log.Fatal("sddf", err)
+	// }
+}
 
 // func main() {
 // 	child_nd1 := node{
