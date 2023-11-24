068b4e2a70bd688353df87d950229bc0dda0785d
Hamza Bow <hamzabow77@gmail.com>
2023-11-24 02:39:24 +0100

    pretty print the whole tree (using json format)

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"which_key_go/node"
 )
@@ -11,5 +12,22 @@ func main() {
 		Description: "Desc of Root",
 		Children:    map[rune]node.Node{},
 	}
-	fmt.Println(rt)
+	rt.Children['a'] = node.Node{
+		Name:        "Child A",
+		Description: "Desc for Child A",
+		Children:    map[rune]node.Node{},
+	}
+	rt.Children['b'] = node.Node{
+		Name:        "Child B",
+		Description: "Desc for Child B",
+		Children:    map[rune]node.Node{},
+	}
+	// fmt.Println(rt)
+	b, _ := json.MarshalIndent(rt, "", "\t")
+	fmt.Println(string(b))
+	var v interface{}
+	err := json.Unmarshal(b, v)
+	fmt.Println("err:", err)
+	fmt.Println(v)
+
 }
