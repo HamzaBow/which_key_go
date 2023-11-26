4f0eea27576080025bea1c8ecb9a9aaf7c11ee27
Hamza Bow <hamzabow77@gmail.com>
2023-11-26 21:47:37 +0100

    remove unused code

--- main.go
+++ main.go
@@ -9,9 +9,4 @@ func main() {
 	var rt = getDefaultTree()
 	b, _ := json.MarshalIndent(rt, "", "\t")
 	fmt.Println(string(b))
-	var v interface{}
-	err := json.Unmarshal(b, v)
-	fmt.Println("err:", err)
-	fmt.Println(v)
-	rt.Children['b'].RunCommand()
 }
