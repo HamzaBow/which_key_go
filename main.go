9b762f43232e980bb827f609a64d526a9f4fa4d1
Hamza Bow <hamzabow77@gmail.com>
2023-12-05 23:19:06 +0100

    reconstruct tree from json file (previous serialization)

--- main.go
+++ main.go
@@ -9,30 +9,41 @@ import (
 )
 
 func main() {
-	rt := getDefaultTree()
-	serializeTree(rt)
+	// rt := getDefaultTree()
+	// serializeTree(rt)
+	var rt node.Node
+	treeBytes, err := os.ReadFile("tree.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(treeBytes, &rt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(rt)
+	rt.PromptPrefixNode()
 }
 
-func serializeTree(rt node.Node) {
+// func serializeTree(rt node.Node) {
 
-	f, err := os.Create("tree.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
+// 	f, err := os.Create("tree.json")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer f.Close()
 
-	// bts, err := json.Marshal(rt)
-	bts, err := json.MarshalIndent(rt, "", "\t")
+// 	// bts, err := json.Marshal(rt)
+// 	bts, err := json.MarshalIndent(rt, "", "\t")
 
-	if err != nil {
-		log.Fatal(err)
-	}
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	_, err = f.Write(bts)
+// 	_, err = f.Write(bts)
 
-	if err != nil {
-		log.Fatal(err)
-	}
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	fmt.Println("done")
-}
+// 	fmt.Println("done")
+// }
