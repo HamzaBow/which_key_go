9f15b6b2d5cb0c71d3efee3c5f957f7aefff363b
Hamza Bow <hamzabow77@gmail.com>
2023-12-05 23:10:46 +0100

    add method for serializing tree

--- main.go
+++ main.go
@@ -1,8 +1,38 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"which_key_go/node"
+)
+
 func main() {
 	rt := getDefaultTree()
-	// rt.PrintSubTree()
-	// rt.Children["c"].PrintParentName()
-	rt.PromptPrefixNode()
+	serializeTree(rt)
+}
+
+func serializeTree(rt node.Node) {
+
+	f, err := os.Create("tree.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	// bts, err := json.Marshal(rt)
+	bts, err := json.MarshalIndent(rt, "", "\t")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = f.Write(bts)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("done")
 }
